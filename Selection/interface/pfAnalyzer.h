050951f1cd62f62d199a3704ca21ec11933b4df9
montanin <>
2012-02-20 10:04:18 +0000

    new PF version

---
+++ Selection/interface/pfAnalyzer.h
@@ -0,0 +1,107 @@
+#ifndef pfAnalyzer_h_
+#define pfAnalyzer_h_
+
+#include <memory>
+#include <string>
+#include <cmath>
+#include <iostream>
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/EgammaCandidates/interface/Electron.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "VecBosSelection/Selection/interface/SelectionUtils.h"
+
+#include "TLorentzVector.h"
+#include "TMath.h"
+#include "TH1.h"
+
+
+class pfAnalyzer : public edm::EDProducer, public SelectionUtils {
+	public:
+		explicit pfAnalyzer(const edm::ParameterSet &);
+		~pfAnalyzer();
+
+		virtual void produce(edm::Event&, edm::EventSetup const&);
+		virtual void beginJob();
+		//virtual bool beginRun(edm::Run &, edm::EventSetup const&);
+
+	private:
+		virtual void endJob() ;
+
+		// ----------member data ---------------------------
+
+		edm::InputTag theElectronCollectionLabel;
+		edm::InputTag pflowEleCollection_;
+		bool removePU_;
+		bool Debug2; //Activate with true if you wonna have verbosity for debug
+		TH1I* passIDEleCriteria;
+		TH1D* eventMultip;
+		TH1F* gsfelEt;
+		TH1D* Conversion;
+		TH1D* Isolation;
+		TH1D* Identification;
+		TH1D* Selected;
+
+		TH1D* eventAccept;
+		TH1D* h_electronEn;
+		TH1D* h_electronEta;
+		TH1D* h_electronPt;
+		TH1D* h_electronInvMass;
+		TH1D* h_electronInvMassPass;
+		bool passSelection;
+ 
+};
+
+
+
+//
+// constructors and destructor
+//
+pfAnalyzer::pfAnalyzer (const edm::ParameterSet & parameters)
+{
+   Debug2 = false;
+   passSelection=true;
+   theElectronCollectionLabel = parameters.getParameter <edm::InputTag> ("electronCollection");
+   pflowEleCollection_ = parameters.getUntrackedParameter<edm::InputTag>("pflowEleCollection",edm::InputTag("particleFlow:electrons"));
+   removePU_ = parameters.getParameter<bool>("removePU");
+   produces<reco::PFCandidateCollection>();
+   
+
+   //Initializations...
+   edm::Service<TFileService> fs;
+   passIDEleCriteria = fs->make<TH1I>("passIDEleCriteria","Ele Id pass/not pass... 3 entries each ele", 3, 0, 3);
+   
+   eventAccept= fs->make<TH1D>("eventAccept","There is corresponding PF", 20, 0, 20);
+   h_electronEn= fs->make<TH1D>("h_electronEn","electronEn", 200, 0, 200);
+   h_electronEta= fs->make<TH1D>("h_electronEta","electronEta", 200, -2.5, 2.5);
+   h_electronPt= fs->make<TH1D>("h_electronPt","electronPt", 200, 0, 200);
+   h_electronInvMass= fs->make<TH1D>("h_electronInvMass","electronInvMass", 60, 60, 120);
+   h_electronInvMassPass= fs->make<TH1D>("h_electronInvMassPass","electronInvMassPass", 60, 60, 120);
+
+}
+
+
+
+pfAnalyzer::~pfAnalyzer ()
+{
+
+	// do anything here that needs to be done at desctruction time
+	// (e.g. close files, deallocate resources etc.)
+
+}
+
+#endif
+
---
+++ Selection/src/pfAnalyzer.cc
@@ -0,0 +1,178 @@
+#include "VecBosSelection/Selection/interface/pfAnalyzer.h"
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+pfAnalyzer::produce(edm::Event & iEvent, edm::EventSetup const & iSetup)
+{
+
+   // get gfs Electron  
+   auto_ptr< reco::PFCandidateCollection > 
+      pOutput( new reco::PFCandidateCollection ); 
+
+  if (Debug2) cout<<"------- NEW Event -----"<<endl;
+  double delta = 0.001;
+  int cont = 0;
+
+  Handle < pat::ElectronCollection > electronCollection;  
+  iEvent.getByLabel (theElectronCollectionLabel, electronCollection);
+  if (electronCollection.isValid () && electronCollection->size()>1){
+ 
+     pat::ElectronCollection::const_iterator highestptele;
+     pat::ElectronCollection::const_iterator secondptele;
+
+     int i=0;
+     bool protection=false;
+     int jj=0;
+     int sizePat=electronCollection->size(); 
+     protection=false;
+     passSelection = true;
+     /// NEW DS
+     // Cutting on WP80
+     for (pat::ElectronCollection::const_iterator recoElectron = electronCollection->begin (); recoElectron != electronCollection->end (); recoElectron++) {
+	
+	protection=true;
+	jj++;
+	//Perform checks on each ele ID criteria
+	// Here you get a plot full of information. Each electron contributes with one entry (so total numer of entries = 3* #electrons)
+	// To have the "%", each bin value shold be divided by total numer of entries/3
+	std::vector<bool> result=SelectionUtils::MakeEleIDAnalysis(recoElectron,iEvent); 
+	if (result[0]) passIDEleCriteria->SetBinContent(1,passIDEleCriteria->GetBinContent(1)+1);
+	if (result[1]) passIDEleCriteria->SetBinContent(2,passIDEleCriteria->GetBinContent(2)+1);
+	if (result[2]) passIDEleCriteria->SetBinContent(3,passIDEleCriteria->GetBinContent(3)+1);
+	
+	if ( SelectionUtils::DoWP80(recoElectron,iEvent) && SelectionUtils::DoHLTMatch(recoElectron,iEvent) && recoElectron->pt()>10.0){	
+	   //if ( SelectionUtils::DoWP80(recoElectron,iEvent) && SelectionUtils::DoHLTMatch(recoElectron,iEvent)){
+	   if (Debug2) cout<<"Tag is a WP80 electron..."<<endl;
+	   
+	   //Sort in Pt
+	   if (Debug2) cout<<"Electron pt value ->"<<recoElectron->pt()<<endl;
+	   if (Debug2) cout<<" MMMM ele trigger size "<<recoElectron->triggerObjectMatches().size()<<endl;
+	   if (i==0) highestptele=recoElectron;
+	   if (i==1){
+	      if (highestptele->pt()<recoElectron->pt()){
+		 secondptele=highestptele;
+		 highestptele=recoElectron;
+	      }
+	      else{
+		 secondptele=recoElectron;
+	      }
+	   }
+	   if (i>1){
+	      if (highestptele->pt()<recoElectron->pt()){
+		 secondptele=highestptele;
+		 highestptele=recoElectron;
+	      }
+	      else{
+		 if (secondptele->pt()<recoElectron->pt()) secondptele=recoElectron;
+	      }
+	   }
+	   i++;
+	}
+	
+	else{
+	   if (Debug2) cout<<"Tag IS a NOT WP80 electron...Exit"<<endl;
+	}
+	
+     }
+     
+     if (!protection) {
+	cout<<"size pat is "<<sizePat<<" while jj is "<<jj<<" and protection "<<protection<<endl;
+	cout<<"problems with PAT collection, in Efficiency.cc-->... Please check..."<<endl;    
+	passSelection = false;
+     }
+     
+     
+     if(i>1 && highestptele->pt()>=20.0) { //you NEED at least two electrons :)
+	
+	//--------------
+	// Match the HLT
+	pat::ElectronCollection::const_iterator tag;
+	pat::ElectronCollection::const_iterator probe;
+	
+	tag=highestptele; //Ã¨ solo un rinominare le cose... (non e' necessario solo retaggio di codice copiato)
+	probe=secondptele;
+	
+	//Calculating Invariant Mass
+	TLorentzVector tagv;
+	tagv.SetPtEtaPhiM(tag->pt(),tag->eta(),tag->phi(), 0.0);
+	TLorentzVector probev;
+	probev.SetPtEtaPhiM(probe->pt(),probe->eta(),probe->phi(), 0.0);
+	
+	TLorentzVector e_pair = tagv + probev;
+	double e_ee_invMass = e_pair.M ();
+	
+	
+	//Cut on the tag and probe mass...
+	if (e_ee_invMass>120 || e_ee_invMass<60) passSelection=false;
+	
+	
+// searching for the corresponding GSF electron 
+	if (passSelection){
+	   Handle<reco::PFCandidateCollection> pfElecCollection;
+	   iEvent.getByLabel (pflowEleCollection_, pfElecCollection);
+	   
+	   for (reco::PFCandidateCollection::const_iterator pfElectron = pfElecCollection->begin (); pfElectron != pfElecCollection->end (); pfElectron++) {
+	      
+	      if ( fabs(pfElectron->pt() - tag->pt())<delta && 
+		   fabs(pfElectron->eta() - tag->eta())<delta &&
+		   fabs(pfElectron->phi() - tag->phi())<delta ) {
+		 pOutput->push_back(*pfElectron);
+		 h_electronEn->Fill(pfElectron->energy());
+		 h_electronEta->Fill(pfElectron->eta());
+		 h_electronPt->Fill(pfElectron->pt());
+		 cont++;
+		 continue;
+	      }
+	      if ( fabs(pfElectron->pt() - probe->pt())<delta && 
+		   fabs(pfElectron->eta() - probe->eta())<delta &&
+		   fabs(pfElectron->phi() - probe->phi())<delta ) {
+		 pOutput->push_back(*pfElectron);
+		 h_electronEn->Fill(pfElectron->energy());
+		 h_electronEta->Fill(pfElectron->eta());
+		 h_electronPt->Fill(pfElectron->pt());
+		 cont++;
+		 continue;
+	      }
+	   }
+	}
+
+	
+	h_electronInvMass->Fill(e_ee_invMass);
+	if (cont==2) h_electronInvMassPass->Fill(e_ee_invMass); 
+     }  // if at least two electron
+  } // if electronCollection is valid
+  
+  eventAccept->Fill(cont);
+  iEvent.put( pOutput );
+  
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+pfAnalyzer::beginJob (){
+
+//beginJob
+
+}
+
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+pfAnalyzer::endJob ()
+{
+
+//endJob
+
+}
+
+DEFINE_FWK_MODULE (pfAnalyzer);
