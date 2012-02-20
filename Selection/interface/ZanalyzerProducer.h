f01382bacbaa9f12ca5075117ff1234e2dc9cae2
montanin <>
2012-02-20 09:59:53 +0000

    minor bug fix

--- Selection/interface/ZanalyzerProducer.h
+++ Selection/interface/ZanalyzerProducer.h
@@ -46,8 +46,20 @@ class ZanalyzerProducer : public edm::EDProducer, public SelectionUtils {
 		edm::InputTag theElectronCollectionLabel;
 		bool removePU_;
 		bool Debug2; //Activate with true if you wonna have verbosity for debug
+		TH1I* passIDEleCriteria;
+		TH1D* eventMultip;
+		TH1F* gsfelEt;
+		TH1D* Conversion;
+		TH1D* Isolation;
+		TH1D* Identification;
+		TH1D* Selected;
 
 		TH1D* eventAccept;
+		TH1D* h_electronEn;
+		TH1D* h_electronEta;
+		TH1D* h_electronPt;
+		TH1D* h_electronInvMass;
+		TH1D* h_electronInvMassPass;
 		bool passSelection;
  
 };
@@ -68,8 +80,14 @@ ZanalyzerProducer::ZanalyzerProducer (const edm::ParameterSet & parameters)
 
    //Initializations...
    edm::Service<TFileService> fs;
+   passIDEleCriteria = fs->make<TH1I>("passIDEleCriteria","Ele Id pass/not pass... 3 entries each ele", 3, 0, 3);
    
-   eventAccept= fs->make<TH1D>("eventAccept","There is no corresponding GSF", 20, 0, 20);
+   eventAccept= fs->make<TH1D>("eventAccept","There is corresponding GSF", 20, 0, 20);
+   h_electronEn= fs->make<TH1D>("h_electronEn","electronEn", 200, 0, 200);
+   h_electronEta= fs->make<TH1D>("h_electronEta","electronEta", 200, -2.5, 2.5);
+   h_electronPt= fs->make<TH1D>("h_electronPt","electronPt", 200, 0, 200);
+   h_electronInvMass= fs->make<TH1D>("h_electronInvMass","electronInvMass", 60, 60, 120);
+   h_electronInvMassPass= fs->make<TH1D>("h_electronInvMassPass","electronInvMassPass", 60, 60, 120);
 
 }
 
