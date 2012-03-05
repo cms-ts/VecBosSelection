de2622b1e513651cbb0b7e0ee70fc137d1e6fbd5
schizzi <>
2012-03-05 14:21:25 +0000

    Added WP80 selection with new HE variable

--- Selection/interface/SelectionUtils.h
+++ Selection/interface/SelectionUtils.h
@@ -12,6 +12,7 @@ class SelectionUtils {
 
 		bool DoWP80(pat::ElectronCollection::const_iterator recoElectron,edm::Event& iEvent);
 		bool DoWP80Pf(pat::ElectronCollection::const_iterator recoElectron,edm::Event& iEvent);
+		bool DoWP80Pf_NewHE(pat::ElectronCollection::const_iterator recoElectron,edm::Event& iEvent);
 		bool DoHLTMatch(pat::ElectronCollection::const_iterator,edm::Event&);
 		std::vector<bool> MakeEleIDAnalysis(pat::ElectronCollection::const_iterator recoElectron,edm::Event& iEvent); 
 
--- Selection/src/SelectionUtils.cc
+++ Selection/src/SelectionUtils.cc
@@ -249,6 +249,131 @@ bool SelectionUtils::DoWP80Pf(pat::ElectronCollection::const_iterator recoElectr
     return false;
 }
 
+//DO the WP80Pf analysis with NEW HE VARIABLE!! (testing!!!)
+bool SelectionUtils::DoWP80Pf_NewHE(pat::ElectronCollection::const_iterator recoElectron,edm::Event& iEvent)
+{
+   double IsoChg = 0;
+   double IsoNeut = 0;
+   double IsoPhot = 0;
+   double IsoTot = 0;
+   double HE=0.;
+   
+   //  if (recoElectron->et () <= 25) return false;
+   
+   // New definition of HE variable (!!!):
+   HE = recoElectron->hcalOverEcalBc();
+
+   if (removePU_){
+      double lepIsoRho;
+      
+      /////// Pileup density "rho" for lepton isolation subtraction /////
+      
+      edm::Handle<double> rhoLepIso;
+      const edm::InputTag eventrhoLepIso("kt6PFJetsForIsolation", "rho");
+      iEvent.getByLabel(eventrhoLepIso, rhoLepIso);
+      if( *rhoLepIso == *rhoLepIso)  lepIsoRho = *rhoLepIso;
+      else  lepIsoRho =  -999999.9;      
+      IsoNeut = (recoElectron->pfIsolationVariables().neutralHadronIso - lepIsoRho*0.096);
+      IsoChg = (recoElectron->pfIsolationVariables().chargedHadronIso - lepIsoRho*0.096);
+      IsoPhot = (recoElectron->pfIsolationVariables().photonIso  - lepIsoRho*0.096);
+      IsoTot = ((IsoNeut + IsoChg + IsoPhot)/ recoElectron->pt ());
+   }
+   else{
+      // Define Isolation variables
+      IsoNeut = recoElectron->pfIsolationVariables().neutralHadronIso;
+      IsoChg = recoElectron->pfIsolationVariables().chargedHadronIso;
+      IsoPhot = recoElectron->pfIsolationVariables().photonIso;
+      IsoTot = ((IsoNeut + IsoChg + IsoPhot)/ recoElectron->pt ());
+   }
+   //Define ID variables
+  
+  float DeltaPhiTkClu = recoElectron->deltaPhiSuperClusterTrackAtVtx ();
+  float DeltaEtaTkClu = recoElectron->deltaEtaSuperClusterTrackAtVtx ();
+  float sigmaIeIe = recoElectron->sigmaIetaIeta ();
+  
+  //Define Conversion Rejection Variables
+  
+  float Dcot = recoElectron->convDcot ();
+  float Dist = recoElectron->convDist ();
+  int NumberOfExpectedInnerHits = recoElectron->gsfTrack ()->trackerExpectedHitsInner ().numberOfHits ();
+  
+  //quality flags
+
+  bool isBarrelElectrons;
+  bool isEndcapElectrons;
+  bool isIsolatedBarrel;
+  bool isIDBarrel;
+  bool isConvertedBarrel;
+  bool isIsolatedEndcap;
+  bool isIDEndcap;
+  bool isConvertedEndcap;
+  isBarrelElectrons = false;
+  isEndcapElectrons = false;
+  isIsolatedBarrel = false;
+  isIDBarrel = false;
+  isConvertedBarrel = false;
+  isIsolatedEndcap = false;
+  isIDEndcap = false;
+  isConvertedEndcap = false;
+ 
+
+/***** Barrel WP80 Cuts *****/
+  
+  if (fabs (recoElectron ->superCluster()->eta()) <= 1.4442) {
+    
+    /* Isolation */
+    if (IsoTot < 0.20) {
+      isIsolatedBarrel = true;
+    }
+    
+    /* Identification */
+    if (fabs (DeltaEtaTkClu) < 0.004 && fabs (DeltaPhiTkClu) < 0.06
+	&& sigmaIeIe < 0.01 && HE < 0.04) {
+      isIDBarrel = true;
+    }
+    
+    /* Conversion Rejection */
+      if ((fabs (Dist) >= 0.02 || fabs (Dcot) >= 0.02)
+	  && NumberOfExpectedInnerHits == 0) {
+	isConvertedBarrel = true;
+      }
+      //cout<<"isIsolatedBarrel "<<isIsolatedBarrel<<" isIDBarrel "<< isIDBarrel<<" isConvertedBarrel "<<isConvertedBarrel<<endl;
+  }
+  
+  if (isIsolatedBarrel && isIDBarrel && isConvertedBarrel) {
+    return true;
+  }
+
+    /***** Endcap WP80 Cuts *****/
+
+    if (fabs (recoElectron ->superCluster()->eta()) >= 1.5660
+	&& fabs (recoElectron ->superCluster()->eta()) <= 2.5000) {
+
+      /* Isolation */  
+      if (IsoTot < 0.20)  {
+	 isIsolatedEndcap = true;
+      }
+
+      /* Identification */
+      if (fabs (DeltaEtaTkClu) < 0.007 && fabs (DeltaPhiTkClu) < 0.03
+	  && sigmaIeIe < 0.03 && HE < 0.15) {
+	isIDEndcap = true;
+      }
+
+      /* Conversion Rejection */
+      if ((fabs (Dcot) > 0.02 || fabs (Dist) > 0.02)
+	  && NumberOfExpectedInnerHits == 0) {
+	isConvertedEndcap = true;
+      }
+      //cout<<"isIsolatedEndcap "<<isIsolatedEndcap<<" isIDEndcap "<< isIDEndcap<<" isConvertedEndcap "<<isConvertedEndcap<<endl;
+    }
+
+    if (isIsolatedEndcap && isIDEndcap && isConvertedEndcap) {
+      return true;
+    }
+    return false;
+}
+
 
 
 
