3f08a27e80ad003bd0a2322c3614f93a285eaa6d
montanin <>
2012-06-05 19:58:35 +0000

    added the photon removal

---
+++ Selection/interface/photonRemoval.h
@@ -0,0 +1,115 @@
+#ifndef photonRemoval_h_
+#define photonRemoval_h_
+
+#include <memory>
+#include <string>
+#include <cmath>
+#include <iostream>
+
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "DataFormats/EgammaCandidates/interface/Electron.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+#include "DataFormats/GsfTrackReco/interface/GsfTrack.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "VecBosSelection/Selection/interface/SelectionUtils.h"
+
+#include "TLorentzVector.h"
+#include "TMath.h"
+#include "TH1.h"
+
+
+class photonRemoval : public edm::EDProducer{
+	public:
+		explicit photonRemoval(const edm::ParameterSet &);
+		~photonRemoval();
+
+		virtual void produce(edm::Event&, edm::EventSetup const&);
+		virtual void beginJob();
+		//virtual bool beginRun(edm::Run &, edm::EventSetup const&);
+
+	private:
+		virtual void endJob() ;		
+		template <class T> bool searchHistory(T const&);
+
+		// ----------member data ---------------------------
+
+		edm::InputTag particleCollectionLabel;
+		edm::InputTag bottomCollectionLabel;
+
+		double barrelRCone;
+		double endcapRCone;
+		TLorentzVector e1_test;
+
+		TH1F * gammaRemovedPt;
+		TH1F * gammaRemovedEta;
+		TH1F * eRemovedPt;
+		TH1F * eRemovedEta;
+ 
+};
+
+
+
+//
+// constructors and destructor
+//
+photonRemoval::photonRemoval (const edm::ParameterSet & parameters)
+{
+   particleCollectionLabel = parameters.getParameter <edm::InputTag> ("particleCollection");
+   bottomCollectionLabel   = parameters.getParameter <edm::InputTag> ("bottomCollection");
+   barrelRCone             = parameters.getUntrackedParameter<double>("barrelRCone",0.05);
+   endcapRCone             = parameters.getUntrackedParameter<double>("endcapRCone",0.07);
+   produces<reco::GenParticleCollection>();
+   
+   //Initializations...
+   edm::Service<TFileService> fs;
+
+   gammaRemovedPt = fs->make<TH1F>("gammaRemovedPt","gammaRemovedPt", 100, 0, 50);
+   gammaRemovedEta = fs->make<TH1F>("gammaRemovedEta","gammaRemovedEta", 100, -2.5, 2.5);
+   eRemovedPt = fs->make<TH1F>("eRemovedPt","eRemovedPt", 100, 0, 100);
+   eRemovedEta = fs->make<TH1F>("eRemovedEta","eRemovedEta", 100, -2.5, 2.5);
+}
+
+
+
+photonRemoval::~photonRemoval ()
+{
+
+	// do anything here that needs to be done at desctruction time
+	// (e.g. close files, deallocate resources etc.)
+
+}
+
+template <class T>
+bool photonRemoval::searchHistory(T const& itSearch)
+{
+   bool pass = false;
+   if (fabs(itSearch->pdgId()) ==11 && itSearch->status()==3 && itSearch->mother()->pdgId()==23) {
+      e1_test.SetPtEtaPhiM(itSearch->pt(),itSearch->eta(),
+                           itSearch->phi(),itSearch->mass());
+
+      pass=true;
+      return pass;
+   } else if (itSearch->status()==3 ){
+      pass=false;
+      return pass;
+   } else {
+      pass=searchHistory(itSearch->mother());
+   }
+   return pass;
+}
+
+
+#endif
+
+
---
+++ Selection/src/photonRemoval.cc
@@ -0,0 +1,182 @@
+#include "VecBosSelection/Selection/interface/photonRemoval.h"
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+
+typedef pair<const reco::GenParticle*, const reco::GenParticle*> eleGamma;
+typedef vector<eleGamma> vecEleGamma;
+
+//
+// member functions
+//
+
+class GreaterPte{
+public:
+  bool operator()( const reco::GenParticle* & a, const reco::GenParticle* & b) {
+     return (a->pt() > b->pt());
+  }
+};
+
+class GreaterPt{
+public:
+  bool operator()( eleGamma & a, eleGamma & b) {
+     return ( (a.first)->pt() > (b.first)->pt() );
+  }
+};
+
+// ------------ method called for each event  ------------
+void
+photonRemoval::produce(edm::Event & iEvent, edm::EventSetup const & iSetup)
+{
+   
+   // get gfs Electron  
+   auto_ptr< reco::GenParticleCollection > 
+      pOutput( new reco::GenParticleCollection ); 
+
+   Handle < reco::GenParticleCollection > particles;  
+   iEvent.getByLabel (particleCollectionLabel, particles);
+   Handle <reco::GenParticleRefVector> bottom;  
+   iEvent.getByLabel (bottomCollectionLabel, bottom); 
+   vecEleGamma gammaCone;
+   gammaCone.clear();
+   vector<const reco::GenParticle*> eVector;
+   eVector.clear();
+
+   // if (particles.isValid ()){
+//       for (reco::GenParticleCollection::const_iterator it = particles->begin (); it != particles->end (); it++) {
+// 	 if (fabs(it->pdgId()==11) && it->status()==1 && searchHistory(&(*it)) ) eVector.push_back(e1_test);
+//       }
+//    }
+   
+   if (particles.isValid ()){
+      for (reco::GenParticleCollection::const_iterator it = particles->begin (); it != particles->end (); it++) {
+	 if (fabs(it->pdgId())==11 && it->status()==1){
+	    // save the electrons
+	    eVector.push_back(&(*it));
+	 }
+      }
+   }
+
+   //stable_sort(eVector.begin(), eVector.end(), GreaterPte());
+
+   if (particles.isValid ()){
+      for (reco::GenParticleCollection::const_iterator it = particles->begin (); it != particles->end (); it++) {
+	 if (it->pdgId()==22 && it->status()==1){
+	    // save the photons
+	    for (vector<const reco::GenParticle*>::const_iterator itEl = eVector.begin(); itEl != eVector.end(); itEl++){
+	       double deltaPhi = fabs(it->phi()- (*itEl)->phi());
+	       if (deltaPhi > acos(-1)) deltaPhi= 2*acos(-1) - deltaPhi;
+	       double deltaR = sqrt( deltaPhi*deltaPhi  + pow(it->eta()-(*itEl)->eta(),2) );
+	       if ((*itEl)->eta()< 1.479 && deltaR < barrelRCone) {
+		  pair<const reco::GenParticle*, const reco::GenParticle*> coppia;
+		  coppia = make_pair (*itEl,&(*it));
+		  gammaCone.push_back(coppia);
+		  break;
+	       } else if ((*itEl)->eta()>= 1.479 && (*itEl)->eta() < 2.5 && deltaR < endcapRCone){
+		  pair<const reco::GenParticle*, const reco::GenParticle*> coppia;
+		  coppia = make_pair (*itEl,&(*it));
+		  gammaCone.push_back(coppia);
+		  break;
+	       } 
+	    }
+	 }
+      }
+   }   
+   
+   //stable_sort(gammaCone.begin(), gammaCone.end(), GreaterPt());
+   //reco::GenParticle* ePlusTmp, eMinusTmp;
+   double ePlusPt=-1, eMinusPt=-1;
+   double ePx=0, ePy=0;
+   int ePlusI=-1, eMinusI=-1, eCont=0;
+   TLorentzVector ePlusP4, eMinusP4, zP4;
+
+   for (vector<const reco::GenParticle*>::const_iterator itEl = eVector.begin(); itEl != eVector.end(); itEl++){
+      ePx=(*itEl)->px(); ePy=(*itEl)->py();
+      for (vecEleGamma::const_iterator it = gammaCone.begin(); it!= gammaCone.end(); it++){
+	 if (it->first == *itEl ){
+	    ePx += (it->second)->px();
+	    ePy += (it->second)->py();
+	 }
+      }
+      if ( (*itEl)->pdgId()==11 && sqrt( ePx*ePx  + ePy*ePy)> ePlusPt ){
+	 ePlusPt = sqrt( ePx*ePx  + ePy*ePy);
+	 //ePlusTmp = *itEl;
+	 ePlusP4.SetPtEtaPhiM((*itEl)->pt(),(*itEl)->eta(),(*itEl)->phi(),(*itEl)->mass());
+	 ePlusI = eCont;
+      }
+      if ( (*itEl)->pdgId()==-11 && sqrt( ePx*ePx  + ePy*ePy)> eMinusPt ){
+	 eMinusPt = sqrt( ePx*ePx  + ePy*ePy);
+	 //eMinusTmp = *itEl;
+	 eMinusP4.SetPtEtaPhiM((*itEl)->pt(),(*itEl)->eta(),(*itEl)->phi(),(*itEl)->mass());
+	 eMinusI = eCont;
+      }
+      eCont++;
+   }
+
+   bool someElectrons = false;
+   if (ePlusI!=-1 && eMinusI!=-1){
+      zP4 = ePlusP4 + eMinusP4;
+      if (zP4.M() > 71 && zP4.M() < 111){
+	 someElectrons = true;
+      }
+   }
+
+	 
+   bool isRemoval = false;
+   for  (reco::GenParticleRefVector::const_iterator itB = bottom->begin (); itB != bottom->end (); itB++) {
+      isRemoval = false;
+      const reco::GenParticle* partB = itB->get();
+      if (someElectrons){
+	 const reco::GenParticle* eleP = eVector[ePlusI];
+	 const reco::GenParticle* eleM = eVector[eMinusI];
+	 // there are electron and gamma to remove
+	 if (partB->pdgId()==22 && partB->status()==1){
+	    // check on the photons
+	    for (vecEleGamma::const_iterator itG = gammaCone.begin (); itG != gammaCone.end (); itG++){
+	       if ((itG->first == eleP || itG->first == eleM) && partB == itG->second) {
+		  isRemoval = true;
+		  gammaRemovedPt->Fill(partB->pt());
+		  gammaRemovedEta->Fill(partB->eta());
+		  break;
+	       }
+	    }
+	 }
+	 if (fabs(partB->pdgId())==11 && partB->status()==1){
+	       if (partB == eleP || partB == eleM) {
+	    // check on the electronsif ((itG->first == eleP || itG->first == eleM) && partB == itG->second) {
+		  isRemoval = true;
+		  eRemovedPt->Fill(partB->pt());
+		  eRemovedEta->Fill(partB->eta());
+		  //break;
+	       }
+	 }
+      } // end someElectrons
+      if (isRemoval == false) pOutput->push_back(*partB);
+   }
+
+   iEvent.put( pOutput );
+   
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+photonRemoval::beginJob (){
+
+//beginJob
+
+}
+
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+photonRemoval::endJob ()
+{
+
+//endJob
+
+}
+
+DEFINE_FWK_MODULE (photonRemoval);
